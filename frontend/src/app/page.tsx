c92edae0fe6c2639ec05538d4cdcc0038d201242
satvik-vinoth <satvikvinoth272@gmail.com>
2025-07-20 18:03:12 +0530

    backend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,10 +6,46 @@ import FrontPage from "@/components/frontpage";
 import dynamic from "next/dynamic";
 const CompanySelector = dynamic(() => import("@/components/CompanySelector"), { ssr: false });
 import ModelSelector from "@/components/ModelSelector";
+import { useEffect } from "react";
 
 
 export default function Home() {
   const [selectedCompany, setSelectedCompany] = useState("AAPL");
+  const [backendReady, setBackendReady] = useState(false);
+  const [checking, setChecking] = useState(true);
+  const baseurl = process.env.NEXT_PUBLIC_API_BASE_URL
+
+  useEffect(() => {
+    const checkBackend = async () => {
+      try {
+        const res = await fetch(`${baseurl}/health`);
+        if (res.ok) {
+          setBackendReady(true);
+          setChecking(false);
+          return;
+        }
+      } catch  {
+        
+      }
+      setTimeout(checkBackend, 3000); 
+    };
+
+    checkBackend();
+  }, []);
+
+  if (!backendReady && checking) {
+    return (
+      <div className="bg-[#1c3b35] min-h-screen flex items-center justify-center text-white text-center p-6">
+        <div>
+          <h2 className="text-2xl font-semibold">Waking up the backend...</h2>
+          <p className="mt-4 text-sm text-gray-300">
+            Due to free-tier limitations, the backend may take 2-3 minutes to wake up. Please wait...
+          </p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     
     <div className="bg-[#1c3b35] min-h-screen relative overflow-hidden">
